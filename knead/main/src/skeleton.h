3760b18bc00d827f38d837089a9e33a1cbf321c2
lubrige <lubrige2@gmail.com>
2020-02-08 19:38:36 +0800

    feat: remove some macros

--- knead/main/src/skeleton.h
+++ knead/main/src/skeleton.h
@@ -13,17 +13,8 @@
 #define ifdebug(x)
 #endif
 #define watch(x) std::cerr << (#x) << " = " << (x) << std::endl
-#define all(x) (x).begin(), (x).end()
-#define mkp make_pair
-#define fr first
-#define sc second
-#define phb(x) push_back(x)
-
-template<typename T>
-constexpr auto sz(T x) { return (x).size(); }
 
 typedef unsigned int uint;
 typedef long long ll;
-typedef std::pair< int, int > pii;
 const double kPi = 2 * std::acos(0.0);
 /* @endsnippet */
